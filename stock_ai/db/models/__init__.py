c60ca13d366b959752943b85deda8691291e1118
johnnychang25678 <zjps3407@gmail.com>
2025-09-16 22:09:47 -0700

    add reddit_posts and reddit_filterd_posts migration

--- stock_ai/db/models/__init__.py
+++ stock_ai/db/models/__init__.py
@@ -1,1 +1,3 @@
 from stock_ai.db.models.test import Test
+from stock_ai.db.models.reddit_post import RedditPost
+from stock_ai.db.models.reddit_filterd_post import RedditFilteredPost
---
+++ stock_ai/db/models/reddit_filterd_post.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from stock_ai.db.base import Base
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Text, Integer, Float, DateTime
+
+class RedditFilteredPost(Base):
+    __tablename__ = "reddit_filtered_posts"
+
+    id: Mapped[str] = mapped_column(String, primary_key=True)
+    run_id: Mapped[str] = mapped_column(String, index=True)
+    flair: Mapped[str] = mapped_column(String)
+    title: Mapped[str] = mapped_column(String)
+    selftext: Mapped[str] = mapped_column(Text)
+    score: Mapped[int] = mapped_column(Integer)
+    num_comments: Mapped[int] = mapped_column(Integer)
+    upvote_ratio: Mapped[float] = mapped_column(Float)
+    created: Mapped[datetime] = mapped_column(DateTime)
+    url: Mapped[str] = mapped_column(String)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
